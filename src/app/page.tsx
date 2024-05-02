7508e333756467e3ab15bd1926524b248202ac9e
AugustoCVS <guty.vertolis@gmail.com>
2024-05-02 12:54:40 -0300

    feat: add login section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,16 @@
+import { CircleDollarSign } from "lucide-react";
+import { Login } from "./auth/login/login.auth";
+
 export default function Home() {
   return (
     <div className="flex items-center justify-center w-screen h-screen">
-      <h1>Hello World!</h1>
+      <div className="w-2/4 bg-gray-700 h-screen flex items-center justify-center">
+        <CircleDollarSign size={120} color="#E1E1E6" />
+      </div>
+
+      <div className="w-2/4 h-screen flex items-center justify-center">
+        <Login />
+      </div>
     </div>
   );
 }
