a9413501748e4fff96842809376017051c7738d0
AugustoCVS <guty.vertolis@gmail.com>
2024-05-02 14:12:22 -0300

    feat: add register component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,14 @@
+"use client";
+
 import { CircleDollarSign } from "lucide-react";
 import { Login } from "./auth/login/login.auth";
+import { useLandingPage } from "./hook";
+import { AnimatePresence } from "framer-motion";
+import { Register } from "./auth/register/register.auth";
 
-export default function Home() {
+export default function LandingPage() {
+  const { states, actions } = useLandingPage();
+
   return (
     <div className="flex items-center justify-center w-screen h-screen">
       <div className="w-2/4 bg-gray-700 h-screen flex items-center justify-center">
@@ -9,8 +16,14 @@ export default function Home() {
       </div>
 
       <div className="w-2/4 h-screen flex items-center justify-center">
-        <Login />
+        <Login handleShowRegister={actions.handleToggleRegister} />
       </div>
+
+      <AnimatePresence>
+        {states.showRegister && (
+          <Register handleCloseRegister={actions.handleToggleRegister} />
+        )}
+      </AnimatePresence>
     </div>
   );
 }
