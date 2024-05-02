09b13ff814e2be25f86e4b5e97e86767a67ec07f
AugustoCVS <guty.vertolis@gmail.com>
2024-05-02 16:04:32 -0300

    feat: create LandingPage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,29 +1,5 @@
-"use client";
+import LandingPage from "./LandingPage/landing-page.component";
 
-import { CircleDollarSign } from "lucide-react";
-import { Login } from "./auth/login/login.auth";
-import { useLandingPage } from "./hook";
-import { AnimatePresence } from "framer-motion";
-import { Register } from "./auth/register/register.auth";
-
-export default function LandingPage() {
-  const { states, actions } = useLandingPage();
-
-  return (
-    <div className="flex items-center justify-center w-screen h-screen">
-      <div className="w-2/4 bg-gray-700 h-screen flex items-center justify-center">
-        <CircleDollarSign size={120} color="#E1E1E6" />
-      </div>
-
-      <div className="w-2/4 h-screen flex items-center justify-center">
-        <Login handleShowRegister={actions.handleToggleRegister} />
-      </div>
-
-      <AnimatePresence>
-        {states.showRegister && (
-          <Register handleCloseRegister={actions.handleToggleRegister} />
-        )}
-      </AnimatePresence>
-    </div>
-  );
+export default function Home() {
+  return <LandingPage />;
 }
