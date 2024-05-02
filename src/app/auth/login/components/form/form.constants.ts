6d5bd1b973505fb5b1e6a750046a258177909604
AugustoCVS <guty.vertolis@gmail.com>
2024-05-02 13:22:13 -0300

    feat: add error messages

--- src/app/auth/login/components/form/form.constants.ts
+++ src/app/auth/login/components/form/form.constants.ts
@@ -1,6 +1,6 @@
 import * as yup from 'yup'
 
 export const signInSchema = yup.object().shape({
-  email: yup.string().email().required(),
-  password: yup.string().required()
+  email: yup.string().email().required('Email é obrigatório'),
+  password: yup.string().required('Senha é obrigatória')
 })
