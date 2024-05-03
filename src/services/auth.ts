3a1be4767812f45b9f4ff75b6ad5708d8b04cba5
AugustoCVS <guty.vertolis@gmail.com>
2024-05-03 20:07:57 -0300

    feat: create authService

---
+++ src/services/auth.ts
@@ -0,0 +1,16 @@
+import { api } from "./api";
+import { LoginRequestProps, LoginResponseProps, RegisterRequestProps } from "./interfaces/auth";
+
+export const AuthService = {
+  register: async (user: RegisterRequestProps) => {
+    const res = await api.post("/users/register", user);
+
+    return res.data;
+  },
+
+  login: async ({email, password}: LoginRequestProps) => {
+    const res = await api.post<LoginResponseProps>("/user/login", {email, password});
+
+    return res.data;
+  }
+}
