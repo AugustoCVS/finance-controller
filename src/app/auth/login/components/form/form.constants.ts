44aadb8886b787f220faf3040642eddcc3a4c9fb
AugustoCVS <guty.vertolis@gmail.com>
2024-05-06 15:31:26 -0300

    feat: create feedback messages

--- src/app/auth/login/components/form/form.constants.ts
+++ src/app/auth/login/components/form/form.constants.ts
@@ -4,3 +4,6 @@ export const signInSchema = yup.object().shape({
   email: yup.string().email("Digite um e-mail válido").required('Email é obrigatório'),
   password: yup.string().required('Senha é obrigatória')
 })
+
+export const SUCCESS_MESSAGE = "Login efetuado com sucesso"
+export const ERROR_MESSAGE = "Credenciais inválidas"
