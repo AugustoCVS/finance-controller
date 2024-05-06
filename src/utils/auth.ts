4cf90e88084e7628a99cda116b5fb91127543d10
AugustoCVS <guty.vertolis@gmail.com>
2024-05-06 17:24:09 -0300

    feat: create functions on auth utils

--- src/utils/auth.ts
+++ src/utils/auth.ts
@@ -1,13 +1,35 @@
 "use server";
 
 import { LoginResponseProps } from "@/services/interfaces/auth";
+import { RequestCookie } from "next/dist/compiled/@edge-runtime/cookies";
 import { cookies } from "next/headers";
 
 const cookiesStore = cookies();
 
 const saveUserInfoOnSessionCookies = (data: LoginResponseProps): void => {
-  cookiesStore.set("@token", data.token, { httpOnly: true, secure: true});
-  cookiesStore.set("@refreshTokenId", data.refreshToken.id, { httpOnly: true, secure: true});
+  cookiesStore.set("@token", data.token, { httpOnly: true, secure: true });
+  cookiesStore.set("@refreshTokenId", data.refreshToken.id, {
+    httpOnly: true,
+    secure: true,
+  });
 };
 
-export { saveUserInfoOnSessionCookies };
+const removeUserInfoOnSessionCookies = (): void => {
+  cookiesStore.delete("@token");
+  cookiesStore.delete("@refreshTokenId");
+};
+
+const getToken = (): RequestCookie | undefined => {
+  return cookiesStore.get("@token");
+};
+
+const getRefreshTokenId = (): RequestCookie | undefined => {
+  return cookiesStore.get("@refreshTokenId");
+};
+
+export {
+  saveUserInfoOnSessionCookies,
+  removeUserInfoOnSessionCookies,
+  getToken,
+  getRefreshTokenId,
+};
