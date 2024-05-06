5e8484f57ccb8b1e051ecc8e874647e854c886ea
AugustoCVS <guty.vertolis@gmail.com>
2024-05-06 15:34:30 -0300

    feat: create handleLogin

--- src/app/auth/login/components/form/form.hook.ts
+++ src/app/auth/login/components/form/form.hook.ts
@@ -1,4 +1,10 @@
+import { AuthService } from "@/services/auth";
+import { LoginRequestProps } from "@/services/interfaces/auth";
+import { MessageUtils } from "@/utils/messages";
+import { useMutation } from "@tanstack/react-query";
 import { useState } from "react";
+import { SUCCESS_MESSAGE } from "./form.constants";
+import { saveUserInfoOnSessionCookies } from "@/utils/auth";
 
 export const useLoginForm = () => {
   const [showPassword, setShowPassword] = useState<boolean>(false);
@@ -7,17 +13,47 @@ export const useLoginForm = () => {
     setShowPassword(!showPassword);
   };
 
-  const handleLogin = (): void => {
-    console.log("Login");
-  }
+  const handleLogin = useMutation({
+    mutationFn: async (data: LoginRequestProps) =>
+      AuthService.login({
+        email: data.email,
+        password: data.password,
+      }),
+    onError: (error) => {
+      MessageUtils.handleSendToast({
+        message: error.message,
+        type: "error",
+      });
+    },
+    onSuccess: (res) => {
+      MessageUtils.handleSendToast({
+        message: SUCCESS_MESSAGE,
+        type: "success",
+      });
+      saveUserInfoOnSessionCookies(res);
+    },
+  });
+
+  const handleChangePassword = (): void => {
+    MessageUtils.handleSendToast({
+      message: "Funcionalidade em desenvolvimento",
+      type: "error",
+    });
+  };
+
+  const onFormSubmit = (data: LoginRequestProps): void => {
+    handleLogin.mutate(data);
+  };
 
   return {
     states: {
       showPassword,
     },
     actions: {
+      handleChangePassword,
       handleShowPassword,
-      handleLogin
-    }
-  }
-}
+      handleLogin,
+      onFormSubmit,
+    },
+  };
+};
