f6f42add7619db0887c4cb05297c6128d40f2a9d
AugustoCVS <guty.vertolis@gmail.com>
2024-05-06 16:11:28 -0300

    feat: change button style

--- src/components/commons/Button/button.component.tsx
+++ src/components/commons/Button/button.component.tsx
@@ -12,7 +12,7 @@ export const Button: React.FC<ButtonProps> = ({
       className={
         className
           ? className
-          : "w-full p-4 border border-gray-500 bg-gray-700 text-gray-100 font-regular hover:bg-gray-100 hover:text-gray-700 rounded-lg"
+          : "w-full items-center justify-center p-4 border border-gray-500 bg-gray-700 text-gray-100 font-regular hover:bg-gray-500 hover:text-gray-700 rounded-lg disabled:bg-gray-500 disabled:hover:text-gray-100 disabled:cursor-not-allowed"
       }
       onClick={onClick}
       disabled={loading}
