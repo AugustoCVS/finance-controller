da19cf79ebf5009441b63ed37d93617626926522
AugustoCVS <guty.vertolis@gmail.com>
2024-05-06 15:34:18 -0300

    feat: implementing input controller

--- src/app/auth/login/components/form/form.component.tsx
+++ src/app/auth/login/components/form/form.component.tsx
@@ -1,14 +1,14 @@
 "use client";
 
-import { Controller, useForm } from "react-hook-form";
+import { useForm } from "react-hook-form";
 import { yupResolver } from "@hookform/resolvers/yup";
 import { Eye, EyeOff } from "lucide-react";
 
 import { signInSchema } from "./form.constants";
 import { FormProps, formProps } from "./form.types";
-import { Input } from "@/components/commons/Input/input.component";
 import { useLoginForm } from "./form.hook";
 import { ButtonSection } from "./components/ButtonSection/button-section.component";
+import { InputController } from "@/components/commons/InputController/input-controller.component";
 
 export const Form: React.FC<FormProps> = ({ handleShowRegister }) => {
   const {
@@ -23,60 +23,44 @@ export const Form: React.FC<FormProps> = ({ handleShowRegister }) => {
 
   const renderPasswordInput = () => {
     return (
-      <div className="relative">
-        <Controller
-          name="password"
-          control={control}
-          render={({ field }) => (
-            <Input
-              placeholder="Senha"
-              type={states.showPassword ? "text" : "password"}
-              value={field.value}
-              onChange={field.onChange}
-              errorMessage={errors.password?.message}
-              isInvalid={!!errors.password}
-            />
-          )}
-        />
-
-        <div
-          className="absolute right-0 top-6 px-4 border-none bg-transparent cursor-pointer"
-          onClick={actions.handleShowPassword}
-        >
-          {states.showPassword ? (
-            <Eye color="#7C7C8A" />
-          ) : (
-            <EyeOff color="#7C7C8A" />
-          )}
-        </div>
-      </div>
+      <InputController
+        name="password"
+        control={control}
+        errorMessage={errors.password?.message}
+        isInvalid={!!errors.password}
+        placeholder="Senha"
+        type={states.showPassword ? "text" : "password"}
+        onClick={actions.handleShowPassword}
+      >
+        {states.showPassword ? (
+          <Eye color="#7C7C8A" />
+        ) : (
+          <EyeOff color="#7C7C8A" />
+        )}
+      </InputController>
     );
   };
 
   return (
-    <form className="flex flex-col item-center gap-4 w-3/4 mt-4">
+    <form
+      className="flex flex-col item-center gap-4 w-3/4 mt-4"
+      onSubmit={handleSubmit(actions.onFormSubmit)}
+    >
       <h1 className="text-gray-900 font-semibold text-lg mb-6 text-center">
         Entrar na sua conta
       </h1>
-      <Controller
+      <InputController
         name="email"
         control={control}
-        render={({ field }) => (
-          <Input
-            placeholder="Email"
-            type="text"
-            value={field.value}
-            onChange={field.onChange}
-            errorMessage={errors.email?.message}
-            isInvalid={!!errors.email}
-          />
-        )}
+        errorMessage={errors.email?.message}
+        isInvalid={!!errors.email}
+        placeholder="Email"
+        type="text"
       />
       {renderPasswordInput()}
       <ButtonSection
-        handleLogin={handleSubmit(actions.handleLogin)}
         handleRegister={handleShowRegister}
-        handleForgotPassword={() => console.log("senha")}
+        handleForgotPassword={actions.handleChangePassword}
         isLoading={false}
       />
     </form>
