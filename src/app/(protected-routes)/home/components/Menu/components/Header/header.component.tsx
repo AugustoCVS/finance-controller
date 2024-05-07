620d3668e7c288a8c14db19d7ef0b1205f1a6247
AugustoCVS <guty.vertolis@gmail.com>
2024-05-07 15:13:41 -0300

    feat: create menu header

---
+++ src/app/(protected-routes)/home/components/Menu/components/Header/header.component.tsx
@@ -0,0 +1,9 @@
+import { CircleDollarSign } from "lucide-react";
+
+export const Header: React.FC = () => {
+  return (
+    <div className="w-full flex items-center justify-center">
+      <CircleDollarSign size={30} color="#E1E1E6" />
+    </div>
+  );
+};
