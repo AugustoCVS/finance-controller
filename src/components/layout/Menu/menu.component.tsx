881aad08784bfc130e8af35c7750f52134b8acfd
AugustoCVS <guty.vertolis@gmail.com>
2024-05-07 15:27:36 -0300

    refactor: change zIndex

--- src/components/layout/Menu/menu.component.tsx
+++ src/components/layout/Menu/menu.component.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { usePathname } from "next/navigation";
+import { usePathname, useRouter } from "next/navigation";
 import { Header } from "./components/Header/header.component";
 import { Options } from "./components/Options/options.component";
 import { Profile } from "./components/Profile/profile.component";
@@ -11,7 +11,7 @@ export const Menu: React.FC = () => {
   if (pathname === "/") return null;
 
   return (
-    <div className="w-20 h-full bg-gray-600 flex flex-col items-center justify-between fixed top-0 left-0 z-50 py-4">
+    <div className="w-20 h-full bg-gray-600 flex flex-col items-center justify-between fixed top-0 left-0 z-10 py-4">
       <div className="w-full flex flex-col items-center">
         <Header />
         <Options />
