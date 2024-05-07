98989b8feec7777d7fff9d7b6b9220c6647b0a94
AugustoCVS <guty.vertolis@gmail.com>
2024-05-07 15:44:20 -0300

    feat: adjust on color

--- src/components/layout/Header/header.component.tsx
+++ src/components/layout/Header/header.component.tsx
@@ -5,7 +5,7 @@ import { useHeader } from "./header.hook";
 export const Header: React.FC = () => {
   const { states, actions } = useHeader();
 
-  if (states.pathName === "") return null;
+  if (states.pathName === "/") return null;
 
   return (
     <header className="w-full h-32 flex items-center justify-center bg-gray-900">
