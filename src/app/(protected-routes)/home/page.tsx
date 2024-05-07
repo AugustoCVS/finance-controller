06da8577824a54c8ce5eda27c31bc9f4ad400e39
AugustoCVS <guty.vertolis@gmail.com>
2024-05-07 15:42:39 -0300

    refactor: adjust

--- src/app/(protected-routes)/home/page.tsx
+++ src/app/(protected-routes)/home/page.tsx
@@ -6,9 +6,5 @@ import { useHome } from "./home.hook";
 export default function HomePage() {
   const { states } = useHome();
 
-  return (
-    <div className="w-full h-screen flex flex-col items-center justify-center">
-      <h1>Home</h1>
-    </div>
-  );
+  return <section className="w-full flex flex-col items-center"></section>;
 }
