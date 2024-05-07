28a2e692c47a557b117615c2d80f1f8b511a2448
AugustoCVS <guty.vertolis@gmail.com>
2024-05-07 15:13:57 -0300

    feat: create section profile on menu

---
+++ src/app/(protected-routes)/home/components/Menu/components/Profile/profile.component.tsx
@@ -0,0 +1,9 @@
+import { CircleUserRound } from "lucide-react";
+
+export const Profile: React.FC = () => {
+  return (
+    <div className="w-full flex items-center justify-center">
+      <CircleUserRound size={30} color="#E1E1E6" />
+    </div>
+  );
+};
