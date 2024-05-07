fb87614231349b26282fe17a5f1911117727cee6
AugustoCVS <guty.vertolis@gmail.com>
2024-05-07 15:16:27 -0300

    feat: add navigation to home

--- src/app/(protected-routes)/home/components/Menu/components/Header/header.component.tsx
+++ src/app/(protected-routes)/home/components/Menu/components/Header/header.component.tsx
@@ -1,9 +1,17 @@
 import { CircleDollarSign } from "lucide-react";
+import { useHeader } from "./header.hook";
 
 export const Header: React.FC = () => {
+  const { actions } = useHeader();
+
   return (
     <div className="w-full flex items-center justify-center">
-      <CircleDollarSign size={30} color="#E1E1E6" />
+      <CircleDollarSign
+        size={30}
+        color="#E1E1E6"
+        onClick={actions.handleNavigateToHome}
+        className="cursor-pointer"
+      />
     </div>
   );
 };
