9e77b7fd591cfacaad11cc194fa4f2ec8b902db5
AugustoCVS <guty.vertolis@gmail.com>
2024-05-09 16:30:29 -0300

    feat: add transactionsCard

--- src/app/(protected-routes)/home/page.tsx
+++ src/app/(protected-routes)/home/page.tsx
@@ -5,15 +5,16 @@ import { useHome } from "./home.hook";
 import { BalanceCards } from "./components/BalanceCards/balance-cards.component";
 import { ButtonSection } from "./components/BalanceCards/components/ButtonSection/button-section.component";
 import { useSummary } from "@/utils/summary";
+import { TransactionCard } from "./components/TransactionCard/transaction-card.component";
 
 export default function HomePage() {
   const { states } = useHome();
   const { balanceList } = useSummary();
 
   return (
-    <section className="w-full h-screen flex flex-col items-center bg-gray-800">
+    <section className="w-full h-screen flex flex-col items-center bg-gray-800 pl-20">
       <ButtonSection />
-      <div className="flex items-center justify-around mt-[-3rem] gap-12">
+      <div className="flex items-center justify-around mt-[-3rem] gap-12 ">
         {balanceList.map((balance) => {
           return (
             <BalanceCards
@@ -21,6 +22,23 @@ export default function HomePage() {
               balance={balance.value}
               title={balance.title}
               isLoading={states.isLoading}
+            />
+          );
+        })}
+      </div>
+
+      <div className="flex flex-col items-center gap-1 mt-8 w-full px-12">
+        {states.transactions.map((transaction) => {
+          return (
+            <TransactionCard
+              key={transaction.id}
+              title={transaction.description}
+              value={transaction.value}
+              accountName={transaction.accountName}
+              category={transaction.category}
+              date={transaction.date}
+              edit={() => {}}
+              remove={() => {}}
             />
           );
         })}
