c7ae59ada4addbe5bf2097a5cb3b64ae0513ad65
AugustoCVS <guty.vertolis@gmail.com>
2024-05-09 13:39:57 -0300

    feat: create transactions types

---
+++ src/services/interfaces/transactions.ts
@@ -0,0 +1,47 @@
+export type MetaData = {
+  page: number;
+  limit: number;
+  total: number;
+};
+
+export enum TransactionType {
+  INCOME = "income",
+  OUTCOME = "outcome",
+}
+
+export enum TransactionCategory {
+  SALARIO = "Salário",
+  ALIMENTACAO = "Alimentação",
+  TRANSPORTE = "Transporte",
+  LAZER = "Lazer",
+  EDUCACAO = "Educação",
+  VIAGENS = "Viagens",
+  SAUDE = "Saúde",
+  OUTROS = "Outros",
+}
+
+export type TransactionsProps = {
+  id: string;
+  description: string;
+  value: number;
+  date: Date;
+  category: TransactionCategory;
+  type: TransactionType;
+  accountId: string;
+  userId: string;
+  createdAt: Date;
+  updatedAt: Date;
+};
+
+export type TransactionRequestProps = {
+  userId?: string;
+  page?: number;
+  limit?: number;
+  accountId?: string;
+  category?: TransactionCategory | "";
+};
+
+export type TransactionResponseProps = {
+  transactions: TransactionsProps[];
+  meta: MetaData;
+};
