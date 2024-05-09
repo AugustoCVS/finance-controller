9e507ae5ed383e0a642d6418dec20384edfc5d3c
AugustoCVS <guty.vertolis@gmail.com>
2024-05-09 15:13:20 -0300

    feat: create handleGetTransactions

--- src/app/(protected-routes)/home/home.hook.ts
+++ src/app/(protected-routes)/home/home.hook.ts
@@ -1,19 +1,44 @@
+import { useDispatch, useSelector } from "react-redux";
+import { RootState } from "@/redux/store";
 import { useQuery } from "@tanstack/react-query";
-import { AuthService } from "@/services/auth";
+
+import { MessageUtils } from "@/utils/messages";
+import { ERROR_MESSAGE } from "./home.constants";
+import { TransactionsService } from "@/services/transactions";
+import { setTransactions } from "@/redux/slices/Transactions/transactions.slice";
 
 export const useHome = () => {
-  const { data } = useQuery({
-    queryKey: ["refreshToken"],
-    queryFn: () => AuthService.profile(),
+  const dispatch = useDispatch();
+  const user = useSelector((state: RootState) => state.user);
+
+  const handleGetTransactions = async (): Promise<void> => {
+    await TransactionsService.getTransactions({
+      userId: user.id,
+    }).then((res) => {
+      dispatch(setTransactions(res.transactions));
+    }).catch(() => {
+      MessageUtils.handleSendToast({
+        message: ERROR_MESSAGE,
+        type: "error",
+      })
+    });
+  };
+
+  const { isLoading } = useQuery({
+    queryKey: ["getBalance"],
+    queryFn: async () => {
+      await handleGetTransactions()
+      return null
+    },
     refetchOnWindowFocus: false,
     retry: false,
-  }); 
-
-  console.log(data)
+    enabled: user.id !== "",
+  });
 
   return {
     states: {
-      data,
+      isLoading,
+      user,
     },
   };
 };
