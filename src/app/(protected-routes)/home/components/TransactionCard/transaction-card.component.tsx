6147805e4158077adcbf615446218c4b2bfdcc0a
AugustoCVS <guty.vertolis@gmail.com>
2024-05-09 16:50:29 -0300

    feat: add skeleton

--- src/app/(protected-routes)/home/components/TransactionCard/transaction-card.component.tsx
+++ src/app/(protected-routes)/home/components/TransactionCard/transaction-card.component.tsx
@@ -1,3 +1,4 @@
+import { SkeletonComponent } from "@/components/commons/Skeleton/skeleton.component";
 import { ButtonSection } from "./components/ButtonSection/button-section.component";
 import { Text } from "./components/Text/text.component";
 import { Value } from "./components/Value/value.component";
@@ -9,9 +10,29 @@ export const TransactionCard: React.FC<TransactionProps> = ({
   title,
   value,
   category,
+  isLoading,
   edit,
   remove,
 }) => {
+  if (isLoading) {
+    return (
+      <div className="w-full flex flex-col">
+        {Array.from({ length: 4 }).map((_, index) => (
+          <div className="w-full">
+            <SkeletonComponent
+              key={index}
+              height={66}
+              width="100%"
+              baseColor="#323238"
+              borderRadius={6}
+              highlightColor="#29292E"
+            />
+          </div>
+        ))}
+      </div>
+    );
+  }
+
   return (
     <div className="w-full flex items-center justify-between bg-gray-600 py-5 px-8 rounded-md">
       <Text text={title} />
