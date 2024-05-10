d487d97941a3732ebf9b57ea88f8aeef6f744737
AugustoCVS <guty.vertolis@gmail.com>
2024-05-10 02:27:37 -0300

    feat: add useDisclosure for new transaction modal

--- src/app/(protected-routes)/home/home.hook.ts
+++ src/app/(protected-routes)/home/home.hook.ts
@@ -6,31 +6,35 @@ import { MessageUtils } from "@/utils/messages";
 import { ERROR_MESSAGE } from "./home.constants";
 import { TransactionsService } from "@/services/transactions";
 import { setTransactions } from "@/redux/slices/Transactions/transactions.slice";
-import { useState } from "react";
+import { useDisclosure } from "@nextui-org/react";
 
 export const useHome = () => {
   const dispatch = useDispatch();
   const user = useSelector((state: RootState) => state.user);
   const transactions = useSelector((state: RootState) => state.transactions);
 
+  const { isOpen, onOpen, onOpenChange } = useDisclosure();
+
   const handleGetTransactions = async (): Promise<void> => {
     await TransactionsService.getTransactions({
       userId: user.id,
-    }).then((res) => {
-      dispatch(setTransactions(res.transactions));
-    }).catch(() => {
-      MessageUtils.handleSendToast({
-        message: ERROR_MESSAGE,
-        type: "error",
+    })
+      .then((res) => {
+        dispatch(setTransactions(res.transactions));
       })
-    });
+      .catch(() => {
+        MessageUtils.handleSendToast({
+          message: ERROR_MESSAGE,
+          type: "error",
+        });
+      });
   };
 
   const { isPending } = useQuery({
     queryKey: ["getBalance"],
     queryFn: async () => {
-      await handleGetTransactions()
-      return null
+      await handleGetTransactions();
+      return null;
     },
     refetchOnWindowFocus: false,
     retry: false,
@@ -41,6 +45,11 @@ export const useHome = () => {
     states: {
       isPending,
       transactions,
+      isOpen,
     },
+    actions: {
+      onOpen,
+      onOpenChange,
+    }
   };
 };
