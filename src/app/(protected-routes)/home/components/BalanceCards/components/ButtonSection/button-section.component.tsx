5fba5d3a652669f1a9cebec8f921e35d1b6c6e10
AugustoCVS <guty.vertolis@gmail.com>
2024-05-10 02:28:36 -0300

    feat: add onClick on button section component

--- src/app/(protected-routes)/home/components/BalanceCards/components/ButtonSection/button-section.component.tsx
+++ src/app/(protected-routes)/home/components/BalanceCards/components/ButtonSection/button-section.component.tsx
@@ -1,10 +1,12 @@
 import { Button } from "@/components/commons/Button/button.component";
 
-export const ButtonSection: React.FC = () => {
+export const ButtonSection: React.FC<{ onClick: () => void }> = ({
+  onClick,
+}) => {
   return (
     <div className="absolute top-4 right-4">
       <Button
-        onClick={() => console.log("Adicionar")}
+        onClick={onClick}
         className="w-full items-center justify-center p-3 border bg-green-500 text-gray-100 font-regular hover:bg-green-300 rounded-lg border-none"
       >
         Nova Transação
