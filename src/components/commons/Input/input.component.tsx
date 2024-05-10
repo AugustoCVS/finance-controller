72797e77795d994d2307e3213c35ca2bf3956877
AugustoCVS <guty.vertolis@gmail.com>
2024-05-10 02:42:24 -0300

    refactor: adjust text color

--- src/components/commons/Input/input.component.tsx
+++ src/components/commons/Input/input.component.tsx
@@ -13,7 +13,7 @@ export const Input: React.FC<InputProps> = ({
   const invalid = !!errorMessage || isInvalid;
 
   const inputStyle = isSecondInput
-    ? "text-gray-500 bg-gray-900 p-4"
+    ? "text-gray-300 bg-gray-900 p-4"
     : "text-gray-800 bg-white p-6 border";
   const hasError = errorMessage ? "border-red-500" : "border-gray-300";
 
@@ -25,7 +25,7 @@ export const Input: React.FC<InputProps> = ({
         value={value}
         onChange={onChange}
         disabled={disabled}
-        className={`flex items-center w-full text-sm text-gray-100 rounded-lg shadow-sm focus:outline-none focus:ring-1 focus:ring-gray-500 focus:border-gray-500
+        className={`flex items-center w-full text-sm rounded-lg shadow-sm focus:outline-none focus:ring-1 focus:ring-gray-500 focus:border-gray-500
           ${inputStyle}
           ${hasError}
         `}
