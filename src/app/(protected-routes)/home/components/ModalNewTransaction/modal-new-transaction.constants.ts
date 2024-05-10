5b6314ad512ed7043accfbcef8ce5f562a9c755a
AugustoCVS <guty.vertolis@gmail.com>
2024-05-10 11:48:19 -0300

    refactor: adjust constants

--- src/app/(protected-routes)/home/components/ModalNewTransaction/modal-new-transaction.constants.ts
+++ src/app/(protected-routes)/home/components/ModalNewTransaction/modal-new-transaction.constants.ts
@@ -1,4 +1,4 @@
-import { TransactionCategory } from "@/services/interfaces/transactions";
+import { TransactionCategory, TransactionType } from "@/services/interfaces/transactions";
 import * as yup from "yup";
 import { FormField } from "./modal-new-transaction.types";
 
@@ -6,9 +6,9 @@ export const NewTransactionSchema = yup.object().shape({
   description: yup.string().required("Descrição do gasto é obrigatória"),
   value: yup.number().required("Valor é obrigatório"),
   date: yup.date().required("Data é obrigatória"),
-  accountName: yup.string().required("Conta é obrigatória"),
-  category: yup.string().required("Categoria é obrigatória"),
-  type: yup.string().required("Tipo da transação é obrigatório"),
+  accountId: yup.string().required("Conta é obrigatória"),
+  category: yup.string<TransactionCategory>().required("Categoria é obrigatória"),
+  type: yup.string<TransactionType>().required("Tipo da transação é obrigatório"),
 });
 
 export const FormFields: FormField[] = [
@@ -30,26 +30,18 @@ export const FormFields: FormField[] = [
     title: "mm/dd/yyyy",
     type: "date",
   },
-  {
-    id: 3,
-    name: "accountName",
-    title: "Conta",
-    type: "select",
-  },
-  {
-    id: 4,
-    name: "category",
-    title: "Categoria",
-    type: "select",
-    options: [
-      { id: 0, value: TransactionCategory.ALIMENTACAO, label: "Alimentação" },
-      { id: 1, value: TransactionCategory.EDUCACAO, label: "Educação" },
-      { id: 2, value: TransactionCategory.LAZER, label: "Lazer" },
-      { id: 3, value: TransactionCategory.SALARIO, label: "Salário" },
-      { id: 4, value: TransactionCategory.SAUDE, label: "Saúde" },
-      { id: 5, value: TransactionCategory.TRANSPORTE, label: "Transporte" },
-      { id: 6, value: TransactionCategory.VIAGENS, label: "Viagens" },
-      { id: 7, value: TransactionCategory.OUTROS, label: "Outros" },
-    ]
-  },
 ];
+
+export const CategoryList = [
+  { id: 0, value: TransactionCategory.ALIMENTACAO, label: "Alimentação" },
+  { id: 1, value: TransactionCategory.EDUCACAO, label: "Educação" },
+  { id: 2, value: TransactionCategory.LAZER, label: "Lazer" },
+  { id: 3, value: TransactionCategory.SALARIO, label: "Salário" },
+  { id: 4, value: TransactionCategory.SAUDE, label: "Saúde" },
+  { id: 5, value: TransactionCategory.TRANSPORTE, label: "Transporte" },
+  { id: 6, value: TransactionCategory.VIAGENS, label: "Viagens" },
+  { id: 7, value: TransactionCategory.OUTROS, label: "Outros" },
+];
+
+export const ERROR_MESSAGE = "Erro ao criar transação";
+export const SUCCESS_MESSAGE = "Transação criada com sucesso";
