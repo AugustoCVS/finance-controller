c05f037fd8f34366957eec86f7b6e9f6141fd08e
AugustoCVS <guty.vertolis@gmail.com>
2024-05-13 20:43:18 -0300

    feat: add logout

--- src/components/layout/Menu/components/Profile/profile.component.tsx
+++ src/components/layout/Menu/components/Profile/profile.component.tsx
@@ -1,8 +1,26 @@
-import { CircleUserRound } from "lucide-react";
+import { deleteUserInfoOnSessionCookies } from "@/utils/auth";
+import { MessageUtils } from "@/utils/messages";
+import { CircleUserRound, LogOut } from "lucide-react";
+import { useRouter } from "next/navigation";
 
 export const Profile: React.FC = () => {
+  const router = useRouter();
+
+  const handleLogOut = () => {
+    deleteUserInfoOnSessionCookies();
+    setTimeout(() => {
+      router.push("/");
+    }, 1000);
+  };
+
   return (
-    <div className="w-full flex items-center justify-center">
+    <div className="w-full flex flex-col items-center justify-center gap-4">
+      <LogOut
+        color="#E1E1E6"
+        size={40}
+        className="cursor-pointer hover:bg-gray-900 p-1 rounded"
+        onClick={handleLogOut}
+      />
       <CircleUserRound size={30} color="#E1E1E6" />
     </div>
   );
