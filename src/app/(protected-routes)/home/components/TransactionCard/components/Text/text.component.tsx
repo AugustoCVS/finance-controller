a84e6b266a28047fd67a78bce331013a8b4e4dda
AugustoCVS <guty.vertolis@gmail.com>
2024-05-13 19:22:14 -0300

    refactor: change date

--- src/app/(protected-routes)/home/components/TransactionCard/components/Text/text.component.tsx
+++ src/app/(protected-routes)/home/components/TransactionCard/components/Text/text.component.tsx
@@ -1,11 +1,12 @@
+import { format } from "date-fns";
 import { TextProps } from "./text.types";
 
 export const Text: React.FC<TextProps> = ({ text, isDate }) => {
   if (isDate) {
+    const transformedDate = format(new Date(text), "MM/dd/yyyy");
+
     return (
-      <p className="w-full flex text-start text-white">
-        {new Date(text).toLocaleDateString()}
-      </p>
+      <p className="w-full flex text-start text-white">{transformedDate}</p>
     );
   }
 
