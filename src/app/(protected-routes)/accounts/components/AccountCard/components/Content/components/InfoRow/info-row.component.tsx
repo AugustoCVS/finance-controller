c7a72b823defdfc815628e5179b3adc0e6750219
AugustoCVS <guty.vertolis@gmail.com>
2024-05-14 12:03:08 -0300

    feat: create info-row component

---
+++ src/app/(protected-routes)/accounts/components/AccountCard/components/Content/components/InfoRow/info-row.component.tsx
@@ -0,0 +1,18 @@
+import { InfoRowProps } from "./info-row.types";
+
+export const InfoRow: React.FC<InfoRowProps> = ({
+  label,
+  value,
+  isDescription,
+}) => {
+  const hasDivider = isDescription ? "mb-16" : "border-b border-gray-900";
+
+  return (
+    <div
+      className={`w-full flex justify-between items-center ${hasDivider} flex-1`}
+    >
+      <span className="text-white text-2xl font-bold">{label}</span>
+      <span className="text-white text-xl flex-1 text-end">{value}</span>
+    </div>
+  );
+};
