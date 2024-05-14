691e6826316f50140e67595edc7d30fe8e54db0c
AugustoCVS <guty.vertolis@gmail.com>
2024-05-14 14:29:05 -0300

    feat: create handleCreateAccount

--- src/app/(protected-routes)/accounts/components/ModalNewAccount/modal-new-account.hook.ts
+++ src/app/(protected-routes)/accounts/components/ModalNewAccount/modal-new-account.hook.ts
@@ -1,14 +1,51 @@
-import { ModalNewAccountProps } from "./modal-new-account.types"
+import { useMutation } from "@tanstack/react-query";
+import { ModalNewAccountProps } from "./modal-new-account.types";
+import { CreateAccountRequestProps } from "@/services/interfaces/account";
+import { AccountService } from "@/services/account";
+import { MessageUtils } from "@/utils/messages";
+import { ERROR_MESSAGE, SUCCESS_MESSAGE } from "./modal-new-account.constants";
 
-export const useModalNewAccount = ({handleGetTransactions, isOpen, onOpenChange, userId}: ModalNewAccountProps) => {
+export const useModalNewAccount = ({
+  handleGetTransactions,
+  isOpen,
+  onOpenChange,
+  userId,
+}: ModalNewAccountProps) => {
   const handleCloseModal = (): void => {
-    onOpenChange()
-  }
+    onOpenChange();
+  };
+
+  const handleCreateAccount = useMutation({
+    mutationFn: async (data: CreateAccountRequestProps) => {
+      await AccountService.createAccount(data, { userId });
+    },
+    onError: () => {
+      MessageUtils.handleSendToast({
+        message: ERROR_MESSAGE,
+        type: "error",
+      });
+    },
+    onSuccess: () => {
+      handleGetTransactions();
+      onOpenChange();
+      MessageUtils.handleSendToast({
+        message: SUCCESS_MESSAGE,
+        type: "success",
+      });
+    },
+  });
+
+  const onFormSubmit = (data: CreateAccountRequestProps) => {
+    handleCreateAccount.mutate(data);
+  };
 
   return {
-    states: {},
+    states: {
+      handleCreateAccount,
+    },
     actions: {
-      handleCloseModal
-    }
-  }
-}
+      handleCloseModal,
+      onFormSubmit,
+    },
+  };
+};
