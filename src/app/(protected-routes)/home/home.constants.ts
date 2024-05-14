d0a27a04a9e3ce5f4e2cf760c42821026ed62ffb
AugustoCVS <guty.vertolis@gmail.com>
2024-05-14 17:31:32 -0300

    feat: create feedback messages for delete

--- src/app/(protected-routes)/home/home.constants.ts
+++ src/app/(protected-routes)/home/home.constants.ts
@@ -1,1 +1,5 @@
 export const ERROR_MESSAGE = "Erro ao buscar transações";
+
+
+export const SUCCESS_DELETE_MESSAGE = "Transação deletada com sucesso";
+export const ERROR_DELETE_MESSAGE = "Erro ao deletar transação";
