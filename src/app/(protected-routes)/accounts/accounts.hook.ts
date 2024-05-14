dca58570e135da08c47f4169cd546ed422a697c9
AugustoCVS <guty.vertolis@gmail.com>
2024-05-14 14:18:23 -0300

    feat: create functions to control the modals

--- src/app/(protected-routes)/accounts/accounts.hook.ts
+++ src/app/(protected-routes)/accounts/accounts.hook.ts
@@ -1,12 +1,17 @@
 import { RootState } from "@/redux/store";
 import { AccountService } from "@/services/account";
+import { useDisclosure } from "@nextui-org/react";
 import { useQuery } from "@tanstack/react-query";
 import { useSelector } from "react-redux";
 
 export const useAccounts = () => {
   const user = useSelector((state: RootState) => state.user);
 
-  const { data, isLoading } = useQuery({
+  const { isOpen, onOpenChange } = useDisclosure();
+  const { isOpen: isEditModalOpen, onOpenChange: onEditModalOpenChange } = useDisclosure();
+  const { isOpen: isDeleteModalOpen, onOpenChange: onDeleteModalOpenChange } = useDisclosure();
+
+  const handleGetAccounts = useQuery({
     queryKey: ["accounts"],
     queryFn: async () => {
       return await AccountService.getAccounts({
@@ -15,10 +20,33 @@ export const useAccounts = () => {
     },
   });
 
+  const handleOpenNewAccountModal = (): void => {
+    onOpenChange();
+  }
+
+  const handleOpenEditModal = (): void => {
+    onEditModalOpenChange();
+  }
+
+  const handleOpenDeleteModal = (): void => {
+    onDeleteModalOpenChange();
+  }
+
   return {
     states: {
-      data,
-      isLoading,
+      handleGetAccounts,
+      user,
+      isOpen,
+      onOpenChange,
+      isEditModalOpen,
+      onEditModalOpenChange,
+      isDeleteModalOpen,
+      onDeleteModalOpenChange,
+    },
+    actions: {
+      handleOpenNewAccountModal,
+      handleOpenEditModal,
+      handleOpenDeleteModal,
     }
   }
 };
