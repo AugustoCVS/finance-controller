59675be253b271ab3ac2765fac2d080b2f64b08c
AugustoCVS <guty.vertolis@gmail.com>
2024-05-14 12:11:22 -0300

    feat: ad pb

--- src/app/(protected-routes)/accounts/components/AccountCard/components/Content/components/InfoRow/info-row.component.tsx
+++ src/app/(protected-routes)/accounts/components/AccountCard/components/Content/components/InfoRow/info-row.component.tsx
@@ -5,11 +5,11 @@ export const InfoRow: React.FC<InfoRowProps> = ({
   value,
   isDescription,
 }) => {
-  const hasDivider = isDescription ? "mb-16" : "border-b border-gray-900";
+  const hasDivider = isDescription ? "mb-12" : "border-b border-gray-900";
 
   return (
     <div
-      className={`w-full flex justify-between items-center ${hasDivider} flex-1`}
+      className={`w-full flex justify-between items-center ${hasDivider} flex-1 pb-4`}
     >
       <span className="text-white text-2xl font-bold">{label}</span>
       <span className="text-white text-xl flex-1 text-end">{value}</span>
