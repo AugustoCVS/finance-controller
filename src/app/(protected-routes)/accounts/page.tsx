65f82dbfa7daf26325be02127231c4f6bb469898
AugustoCVS <guty.vertolis@gmail.com>
2024-05-13 23:22:30 -0300

    feat: start to create accounts screen

---
+++ src/app/(protected-routes)/accounts/page.tsx
@@ -0,0 +1,9 @@
+"use client";
+
+export default function Accounts() {
+  <>
+    <section className="w-full h-full min-h-screen flex flex-col items-center bg-gray-800 pl-20 pb-8">
+      <h1>Accounts</h1>
+    </section>
+  </>;
+}
