f15bb0a6ce4083cf25b03772225d75f494f652dd
AugustoCVS <guty.vertolis@gmail.com>
2024-05-14 12:03:58 -0300

    feat: add AccountCard

--- src/app/(protected-routes)/accounts/page.tsx
+++ src/app/(protected-routes)/accounts/page.tsx
@@ -1,9 +1,22 @@
 "use client";
 
+import { AccountType, BankTypes } from "@/services/interfaces/account";
+import { AccountCard } from "./components/AccountCard/account-card.component";
+
 export default function Accounts() {
-  <>
-    <section className="w-full h-full min-h-screen flex flex-col items-center bg-gray-800 pl-20 pb-8">
-      <h1>Accounts</h1>
-    </section>
-  </>;
+  return (
+    <>
+      <section className="w-full h-full min-h-screen flex flex-col items-center bg-gray-800 pl-20 pb-8">
+        <div className="w-full flex items-center justify-center pt-12">
+          <AccountCard
+            name="Nubank - Corrente"
+            accountType={AccountType.CORRENTE}
+            balance={1000}
+            bank={BankTypes.NUBANK}
+            description="Conta para gastos mensais"
+          />
+        </div>
+      </section>
+    </>
+  );
 }
