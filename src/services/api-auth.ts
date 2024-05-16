8017443e3ae328dab75759a04fc7525fc0342320
AugustoCVS <guty.vertolis@gmail.com>
2024-05-16 11:01:22 -0300

    feat: update on env

--- src/services/api-auth.ts
+++ src/services/api-auth.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 import { responseInterceptor } from "./interceptors/ResponseInterceptor";
 import { errorInterceptor } from "./interceptors/ErrorInterceptor";
 
-const BASE_URL = process.env.BASE_URL;
+const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL;
 
 const apiAuth = axios.create({
   baseURL: BASE_URL,
